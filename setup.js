2ab97e50265fed192ac727d7e359da2dbca6f8c2
LagomY <lagomscripts@gmail.com>
2024-05-23 06:09:53 +0100

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Lagom
+// @namespace    https://gist.github.com/LagomY
+// @version      1.3
+// @description  flw:0.0.0.0.1 lagomscripts@gmail.com
+// @author       Lagom
+// @license      AGPL-3.0-or-later
+// @copyright    2024, Lagom (https://github.com/LagomY)
+// @match        http://*/*
+// @match        https://*/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        unsafeWindow
+// ==/UserScript==
+
+
